3b85d38c8fca22121ba0805663abf85c1cc5e916
Wombatlord <jordan.stone0@gmail.com>
2022-07-08 04:42:11 +0100

    Alternate render function for use with grayscale ramp ASCII charsets.

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"image"
 	"image/color"
+
 	_ "image/jpeg"
 	_ "image/png"
 	"io/ioutil"
@@ -32,20 +33,24 @@ func RGB(r, g, b byte, p Painter) string {
 	return fmt.Sprintf("%s2;%d;%d;%dm", p, r, g, b)
 }
 
-type Mode int
+type Charset int
 
 const (
-	Normal Mode = iota
+	Normal Charset = iota
 	TurboGFX
 	ASCIIFY
 )
 
-// Palettes is the mapping of glyph to brightness levels
-// indexed by Mode
-var Palettes = [3][5]string{
+// Charsets is the mapping of glyph to brightness levels
+// indexed by Charset Arg.
+var Charsets = [3][5]string{
 	Normal:   {"█", "█", "█", "█", "█"},
 	TurboGFX: {" ", "░", "▒", "▓", "█"},
-	ASCIIFY: {" ",".","*","$","@"},
+	ASCIIFY:  {" ", ".", "*", "$", "@"},
+}
+
+var Ramps = []string{
+	Normal: "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,^`'. ",
 }
 
 const NotSet = 0
@@ -55,7 +60,8 @@ type Cli struct {
 	Scale    float64 `arg:"-s, --scale" help:"overall image scale" default:"1.0"`
 	Squash   float64 `arg:"-w, --wide-boyz" help:"How wide you want it guv? (Widens the image)" default:"1.0"`
 	StdInput bool    `arg:"-i, --in" help:"read from stdin"`
-	Mode     Mode    `arg:"-m, --mode" help:"mode selection determines characters used for rendering" default:"0"`
+	Mode     string  `arg:"-m, --mode" help:"mode selection determines renderer" default:"A"`
+	Charset  Charset `arg:"-p, --palette" help:"palette selection determines the character set used by the renderer" default:"0"`
 	YOrigin  int     `arg:"--y-org" help:"minimum Y, top of focus" default:"0"`
 	Height   int     `arg:"--height" help:"height, vertical size of focus" default:"0"`
 	XOrigin  int     `arg:"--x-org" help:"minimum X, left edge of focus" default:"0"`
@@ -64,7 +70,7 @@ type Cli struct {
 }
 
 func (c Cli) GetPath() string    { return c.Path }
-func (c Cli) GetStdIn() bool  { return c.StdInput }
+func (c Cli) GetStdIn() bool     { return c.StdInput }
 func (c Cli) GetScale() float64  { return c.Scale }
 func (c Cli) GetSquash() float64 { return c.Squash }
 func (c Cli) GetYOrigin() int    { return c.YOrigin }
@@ -86,7 +92,7 @@ func (c *Cli) GetFocusView(img image.Image) FocusView {
 
 	c.Height = util.Min(c.Height, img.Bounds().Max.Y)
 	c.YOrigin = util.Min(c.YOrigin, img.Bounds().Max.Y-c.Height)
-	
+
 	return c
 }
 
@@ -136,7 +142,7 @@ func loadImage(ps PathSpec) (img image.Image, err error) {
 		}
 	}
 	defer imgFile.Close()
-	
+
 	img, _, err = image.Decode(imgFile)
 	if err != nil {
 		return nil, err
@@ -162,10 +168,26 @@ func ScaleImg(img image.Image, sf ScaleFactors) image.Image {
 	return resize.Resize(w, h, img, resize.Lanczos2)
 }
 
+func grayscale(c color.Color) int {
+	r, g, b, _ := c.RGBA()
+	return int(0.299*float64(r) + 0.587*float64(g) + 0.114*float64(b))
+}
+
+func avgPixel(img image.Image, x, y, w, h int) int {
+	cnt, sum, max := 0, 0, img.Bounds().Max
+	for i := x; i < x+w && i < max.X; i++ {
+		for j := y; j < y+h && j < max.Y; j++ {
+			sum += grayscale(img.At(i, j))
+			cnt++
+		}
+	}
+	return sum / cnt
+}
+
 // PrintImg is the stdout of the program, i.e. Sick GFX.
-func PrintImg(mode Mode, focusView FocusView, img image.Image) {
-	glyphs := Palettes[mode]
-	
+func PrintImg(charset Charset, focusView FocusView, img image.Image) {
+	glyphs := Charsets[charset]
+
 	// img relative x, y pixel lower bounds
 	top, left := focusView.GetYOrigin(), focusView.GetXOrigin()
 
@@ -181,6 +203,7 @@ func PrintImg(mode Mode, focusView FocusView, img image.Image) {
 
 			// get brightness of cell
 			c := color.GrayModel.Convert(img.At(x, y)).(color.Gray)
+
 			level := c.Y / 51 // 51 * 5 = 255
 			if level == 5 {   // clipping
 				level--
@@ -199,19 +222,84 @@ func PrintImg(mode Mode, focusView FocusView, img image.Image) {
 
 		fmt.Println(Normalizer)
 	}
-} 
+}
+
+func tdPalette(img image.Image, focusView FocusView, sf ScaleFactors) {
+	// pal := make([]byte, 256)
+
+	// // setup a palette mapping
+	// // palette: ABCDEFGHIJKLMNOP
+	// var chunkIdx byte = 0
+	// for len(pal) < 256 {
+	// 	//chunk := bytes.Repeat([]byte{0x41+chunkIdx}, 16)
+	// 	for i := 0; i < 16; i++ {
+	// 		chunkOffset := int(chunkIdx)*16
+
+	// 		idx := chunkOffset + i
+	// 		if idx > 255 {
+	// 			log.Fatalln("too big: ", idx)
+	// 		}
+	// 		pal[idx] = 0x41+chunkIdx
+	// 	}
+	// 	chunkIdx++
+	// }
+
+	// fmt.Printf("palette length: %d\n", len(pal))
+	// if len(pal) > 256 {
+	// 	log.Fatalf("Palette was too long!\n")
+	// }
+	// fmt.Println(pal)
+}
+
+func PaletteTesting(charset Charset, focusView FocusView, img image.Image) {
+	glyphs := Ramps[charset]
+
+	// img relative x, y pixel lower bounds
+	top, left := focusView.GetYOrigin(), focusView.GetXOrigin()
+
+	// img relative x, y pixel upper bounds (right, top)
+	right := focusView.GetXOrigin() + focusView.GetWidth()
+	btm := focusView.GetYOrigin() + focusView.GetHeight()
+
+	scaleY := 1
+
+	// go row by row in the scaled image.Image and...
+	for y := top; y < btm; y += int(Args.Squash) {
+
+		// print cells from left to right
+		for x := left; x < right; x += scaleY {
+			// get brightness of cell
+			c := avgPixel(img, x, y, int(Args.Squash), scaleY)
+
+			// get the rgba colour
+			rgb := rotato.RotateHue(color.RGBAModel.Convert(img.At(x, y)).(color.RGBA), Args.HueAngle)
+
+			// get the colour and glyph corresponding to the brightness
+			ink := RGB(rgb.R, rgb.G, rgb.B, Foreground)
+
+			fmt.Print(ink + string(glyphs[len(glyphs)*c/65536]))
+		}
+		fmt.Println()
+	}
+}
 
 func main() {
 	arg.MustParse(&Args)
 	ArgsToJson(Args)
-	
+
 	img, err := loadImage(Args)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	img = ScaleImg(img, Args)
-
 	focusView := Args.GetFocusView(img)
-	PrintImg(Args.Mode, focusView, img)
+
+	switch {
+	case Args.Mode == "A":
+		PrintImg(Args.Charset, focusView, img)
+
+	case Args.Mode == "B":
+		PaletteTesting(Args.Charset, focusView, img)
+	}
 }
