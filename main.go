ed8958b9474617e9da02462a758735c4ea677680
Wombatlord <jordan.stone0@gmail.com>
2022-07-13 04:46:15 +0100

    Mp4ToFrames() moved to photerm package

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ import (
 	_ "image/png"
 	"log"
 	"os"
-	"os/exec"
 	"strings"
 
 	"wombatlord/imagestuff/photerm"
@@ -239,22 +238,6 @@ func PrintFromBuff(imageBuffer <-chan image.Image, glyphs string) (err error) {
 	return nil
 }
 
-func mp4ToFrames() {
-	// Split path into constituent strings
-	dest := strings.Split(Args.Path, "/")
-	// re-join all but the final element to construct the path minus the target mp4
-	destDir := strings.Join(dest[0:len(dest)-1], "/")
-
-	// construct the ffmpeg command & run it to convert mp4 to indvidual images saved in destDir
-	// images are named in ascending order, starting at 00000.jpg
-	c := exec.Command("ffmpeg", "-i", Args.Path, "-vf", "fps=24", destDir+"/%05d.jpg")
-	err := c.Run()
-	// catch any errors from the ffmpeg call.
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
 func main() {
 
 	arg.MustParse(&Args)
@@ -270,7 +253,7 @@ func main() {
 		// Provide a path to an mp4.
 		// it will be converted to individual jpgs.
 		// jpgs will be saved in the same directory as the mp4.
-		mp4ToFrames()
+		photerm.Mp4ToFrames(Args)
 		fallthrough
 
 	case "R":
--- photerm/photerm.go
+++ photerm/photerm.go
@@ -2,6 +2,9 @@ package photerm
 
 import (
 	"image"
+	"log"
+	"os/exec"
+	"strings"
 
 	"github.com/nfnt/resize"
 )
@@ -26,6 +29,8 @@ type PathSpec interface {
 	GetStdIn() bool
 }
 
+// Region fields define the area of an image that will be rendered.
+// Essentially crops the image post scaling if values are non-default.
 type Region struct{ Left, Top, Right, Btm int }
 
 
@@ -44,4 +49,21 @@ func OutputDimsOf(scales ScaleFactors, img image.Image) (w, h uint) {
 func ScaleImg(img image.Image, sf ScaleFactors) image.Image {
 	w, h := OutputDimsOf(sf, img)
 	return resize.Resize(w, h, img, resize.Lanczos2)
+}
+
+// Proxy to ffmpeg CLI for generating sequential jpgs from an mp4.
+func Mp4ToFrames(p PathSpec) {
+	// Split path into constituent strings
+	dest := strings.Split(p.GetPath(), "/")
+	// re-join all but the final element to construct the path minus the target mp4
+	destDir := strings.Join(dest[0:len(dest)-1], "/")
+
+	// construct the ffmpeg command & run it to convert mp4 to indvidual images saved in destDir
+	// images are named in ascending order, starting at 00000.jpg
+	c := exec.Command("ffmpeg", "-i", p.GetPath(), "-vf", "fps=24", destDir+"/%05d.jpg")
+	err := c.Run()
+	// catch any errors from the ffmpeg call.
+	if err != nil {
+		log.Fatal(err)
+	}
 }
