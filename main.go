9a6f551698aad648836c10d130c38e6a840f9a82
Wombatlord <jordan.stone0@gmail.com>
2022-07-13 04:04:30 +0100

    Following refactor principles from custom-palette branch.

--- main.go
+++ main.go
@@ -12,11 +12,11 @@ import (
 	"os/exec"
 	"strings"
 
+	"wombatlord/imagestuff/photerm"
 	"wombatlord/imagestuff/src/rotato"
 	"wombatlord/imagestuff/src/util"
 
 	"github.com/alexflint/go-arg"
-	"github.com/nfnt/resize"
 )
 
 type Painter string
@@ -62,6 +62,7 @@ type Cli struct {
 	StdInput bool    `arg:"-i, --in" help:"read from stdin"`
 	Mode     string  `arg:"-m, --mode" help:"mode selection determines renderer" default:"A"`
 	Charset  Charset `arg:"-c, --Charset" help:"Charset selection determines the character set used by the renderer" default:"0"`
+	Custom   string  `arg:"--custom" help:"provide a custom string to render with" default:"█"`
 	YOrigin  int     `arg:"--y-org" help:"minimum Y, top of focus" default:"0"`
 	Height   int     `arg:"--height" help:"height, vertical size of focus" default:"0"`
 	XOrigin  int     `arg:"--x-org" help:"minimum X, left edge of focus" default:"0"`
@@ -77,8 +78,16 @@ func (c Cli) GetYOrigin() int    { return c.YOrigin }
 func (c Cli) GetHeight() int     { return c.Height }
 func (c Cli) GetXOrigin() int    { return c.XOrigin }
 func (c Cli) GetWidth() int      { return c.Width }
+func (c Cli) GetRegion() photerm.Region {
+	return photerm.Region{
+		Left:  c.GetXOrigin(),
+		Top:   c.GetYOrigin(),
+		Right: c.GetXOrigin() + c.GetWidth(),
+		Btm:   c.GetYOrigin() + c.GetHeight(),
+	}
+}
 
-func (c *Cli) GetFocusView(img image.Image) FocusView {
+func (c *Cli) GetFocusView(img image.Image) photerm.FocusView {
 	// set defaults as dynamic image size
 	if c.Height == NotSet {
 		c.Height = img.Bounds().Max.Y
@@ -98,23 +107,6 @@ func (c *Cli) GetFocusView(img image.Image) FocusView {
 
 var Args Cli
 
-type FocusView interface {
-	GetYOrigin() int
-	GetHeight() int
-	GetXOrigin() int
-	GetWidth() int
-}
-
-type ScaleFactors interface {
-	GetScale() float64
-	GetSquash() float64
-}
-
-type PathSpec interface {
-	GetPath() string
-	GetStdIn() bool
-}
-
 var (
 	imgFile *os.File
 )
@@ -128,87 +120,53 @@ func ArgsToJson(c Cli) {
 	_ = os.WriteFile(fileName, file, 0644)
 }
 
-// OutputBoundsOf consumes a Cli value and returns pixel width, height tuple
-func OutputDimsOf(scales ScaleFactors, img image.Image) (w, h uint) {
-	height := float64(uint(img.Bounds().Max.Y))
-	width := float64(uint(img.Bounds().Max.X))
-
-	scale := scales.GetScale()
-	ratio := width / height * scales.GetSquash()
-
-	return uint(scale * height * ratio), uint(scale * height)
-}
-
-// ScaleImg does global scale and makes boyz wide
-func ScaleImg(img image.Image, sf ScaleFactors) image.Image {
-	w, h := OutputDimsOf(sf, img)
-	return resize.Resize(w, h, img, resize.Lanczos2)
-}
-
-func Grayscale(c color.Color) int {
-	r, g, b, _ := c.RGBA()
-	return int(0.299*float64(r) + 0.587*float64(g) + 0.114*float64(b))
-}
-
-func AvgPixel(img image.Image, x, y, w, h int) int {
-	cnt, sum, max := 0, 0, img.Bounds().Max
-	for i := x; i < x+w && i < max.X; i++ {
-		for j := y; j < y+h && j < max.Y; j++ {
-			sum += Grayscale(img.At(i, j))
-			cnt++
-		}
-	}
-	return sum / cnt
-}
-
-func FocusArea(focusView FocusView) (top, left, right, btm int) {
-	// img relative x, y pixel lower bounds
-	top, left = focusView.GetYOrigin(), focusView.GetXOrigin()
-
-	// img relative x, y pixel upper bounds (right, top)
-	right = focusView.GetXOrigin() + focusView.GetWidth()
-	btm = focusView.GetYOrigin() + focusView.GetHeight()
-
-	return top, left, right, btm
-}
-
 // BufferImageDir runs asynchronously to load files into memory
 // Each file is sent into imageBuffer to be consumed elsewhere.
-func BufferImageDir(imageBuffer chan image.Image, fs []os.DirEntry) (err error) {
+// This is an example of a generator pattern in golang.
+func BufferImageDir(fs []os.DirEntry) <-chan image.Image {
 
-	for _, file := range fs {
+	// Define the asynchronous work that will process data and populate the generator
+	// Anonymous func takes the write side of a channel
+	work := func(results chan<- image.Image) {
+		for _, file := range fs {
 
-		// Ignore serialised args file and proceed with iteration
-		ext := strings.Split(file.Name(), ".")[1]
+			// Ignore serialised args file and proceed with iteration
+			ext := strings.Split(file.Name(), ".")[1]
 
-		switch ext {
-		case "json":
-			continue
-		case "mp4":
-			continue
+			switch ext {
+			case "json":
+				continue
+			case "mp4":
+				continue
+			}
+
+			imgFile, err := os.Open(fmt.Sprintf("%s%s", Args.GetPath(), file.Name()))
+
+			if err != nil {
+				log.Fatalf("LOAD ERR: %s", err)
+			}
+
+			img, _, err := image.Decode(imgFile)
+			_ = imgFile.Close()
+
+			if err != nil {
+				log.Fatalf("DECODE ERR: %s", err)
+			}
+			// Scale image, then read image.Image into channel.
+			img = photerm.ScaleImg(img, Args)
+			results <- img
 		}
-
-		imgFile, err = os.Open(fmt.Sprintf("%s%s", Args.GetPath(), file.Name()))
-
-		if err != nil {
-			log.Fatalf("LOAD ERR: %s", err)
-		}
-
-		defer imgFile.Close()
-
-		img, _, err := image.Decode(imgFile)
-
-		if err != nil {
-			log.Fatalf("DECODE ERR: %s", err)
-		}
-		// Scale image, then read image.Image into channel.
-		img = ScaleImg(img, Args)
-		imageBuffer <- img
+		// Close the channel once all files have been read into it.
+		close(results)
 	}
 
-	// Close the channel once all files have been read into it.
-	close(imageBuffer)
-	return nil
+	// blocking code
+	imageBuffer := make(chan image.Image, len(fs))
+	// non-blocking
+	go work(imageBuffer)
+
+	// Return the read side of the channel
+	return imageBuffer
 }
 
 // Buffer a single image for non-sequential display
@@ -228,7 +186,7 @@ func BufferImagePath(imageBuffer chan image.Image) (err error) {
 		log.Fatalf("DECODE ERR: %s", err)
 	}
 	// Scale image, then read image.Image into channel.
-	img = ScaleImg(img, Args)
+	img = photerm.ScaleImg(img, Args)
 	imageBuffer <- img
 	close(imageBuffer)
 	return nil
@@ -238,31 +196,35 @@ func BufferImagePath(imageBuffer chan image.Image) (err error) {
 // This function prints the buffer sequentially.
 // Essentially, this is lo-fi in-terminal video playback via UTF-8 / ASCII encoded pixels.
 // For now, use ffmpeg cli to generate frames from a video file.
-func PrintFromBuff(imageBuffer chan image.Image, charset Charset) (err error) {
+func PrintFromBuff(imageBuffer <-chan image.Image, glyphs string) (err error) {
 	var frame string
-	glyphs := Charsets[charset]
+
+	palette := [256]rune{}
+	copy(palette[:], []rune(util.Stretch(glyphs, 255)))
 
 	for img := range imageBuffer {
-		
+
 		scaleY := 1
-		
+
 		focusView := Args.GetFocusView(img)
-		top, left, right, btm := FocusArea(focusView)
+		r := focusView.GetRegion()
+		//top, left, right, btm := FocusArea(focusView)
 		// go row by row in the scaled image.Image and...
-		for y := top; y < btm; y += int(Args.Squash) {
+		for y := r.Top; y < r.Btm; y += int(Args.Squash) {
 			frame = ""
 			// print cells from left to right
-			for x := left; x < right; x += scaleY {
+			for x := r.Left; x < r.Right; x += scaleY {
 				// get brightness of cell
-				c := AvgPixel(img, x, y, int(Args.Squash), scaleY)
-				
+				//c := AvgPixel(img, x, y, int(Args.Squash), scaleY)
+				c := color.GrayModel.Convert(img.At(x, y)).(color.Gray).Y
+
 				// get the rgba colour
 				rgb := rotato.RotateHue(color.RGBAModel.Convert(img.At(x, y)).(color.RGBA), Args.HueAngle)
-				
+
 				// get the colour and glyph corresponding to the brightness
 				ink := RGB(rgb.R, rgb.G, rgb.B, Foreground)
-				
-				frame += ink + string(glyphs[len(glyphs)*c/65536])
+
+				frame += ink + string(palette[c])
 			}
 			frame += Normalizer + "\n"
 			fmt.Print(frame)
@@ -271,7 +233,7 @@ func PrintFromBuff(imageBuffer chan image.Image, charset Charset) (err error) {
 		if len(imageBuffer) == 0 {
 			fmt.Println(Normalizer) // leave the final frame in the terminal. Allows for single image render.
 		} else {
-			fmt.Printf("\033[%sA", fmt.Sprint(btm)) // reset cursor to original position before drawing next image.
+			fmt.Printf("\033[%sA", fmt.Sprint(r.Btm)) // reset cursor to original position before drawing next image.
 		}
 	}
 	return nil
@@ -291,52 +253,47 @@ func mp4ToFrames() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	
-	// maybe we can do something with buffering output directly to memory instead of saving?
-	// https://stackoverflow.com/questions/66002676/how-can-i-get-the-file-output-of-the-ffmpeg-command
-	// buff := make([]byte, 5000)
-	// n, err := c.Stdout.Write(buff)
-
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// fmt.Printf("buff: %v\n", n)
 }
 
 func main() {
+
 	arg.MustParse(&Args)
 	ArgsToJson(Args)
 
+	charset := strings.Join(Charsets[Args.Charset], "")
+	if Args.Custom != "" {
+		charset = Args.Custom
+	}
+
 	switch Args.Mode {
+	case "L":
+		// Provide a path to an mp4.
+		// it will be converted to individual jpgs.
+		// jpgs will be saved in the same directory as the mp4.
+		mp4ToFrames()
+		fallthrough
 
-	case "A":
-		// Provide a full path to Mode A for individual image display.
-
-		imageBuffer := make(chan image.Image, 1)
-		go BufferImagePath(imageBuffer)
-		PrintFromBuff(imageBuffer, Args.Charset)
-
-	case "B":
+	case "R":
 		// Provide a DIRECTORY to Mode B for sequential play of all images inside.
 
 		fs, err := os.ReadDir(Args.GetPath())
 		if err != nil {
 			log.Fatal(err)
 		}
-		imageBuffer := make(chan image.Image, len(fs))
 
 		// load image files in a goroutine
 		// ensures playback is not blocked by io.
-		go BufferImageDir(imageBuffer, fs)
+		imageBuffer := BufferImageDir(fs)
 
 		// Consumes image.Image from imageBuffer
 		// Prints each to the terminal.
-		PrintFromBuff(imageBuffer, Args.Charset)
-	
-	case "C":
-		// Provide a path to an mp4.
-		// it will be converted to individual jpgs.
-		// jpgs will be saved in the same directory as the mp4.
-		mp4ToFrames()
+		PrintFromBuff(imageBuffer, charset)
+
+	case "I":
+		// Provide a full path to Mode A for individual image display.
+
+		imageBuffer := make(chan image.Image, 1)
+		BufferImagePath(imageBuffer)
+		PrintFromBuff(imageBuffer, charset)
 	}
 }
---
+++ photerm/photerm.go
@@ -0,0 +1,47 @@
+package photerm
+
+import (
+	"image"
+
+	"github.com/nfnt/resize"
+)
+
+// These are interfaces to the CLI struct
+// for interacting with its fields.
+type FocusView interface {
+	GetYOrigin() int
+	GetHeight() int
+	GetXOrigin() int
+	GetWidth() int
+	GetRegion() Region
+}
+
+type ScaleFactors interface {
+	GetScale() float64
+	GetSquash() float64
+}
+
+type PathSpec interface {
+	GetPath() string
+	GetStdIn() bool
+}
+
+type Region struct{ Left, Top, Right, Btm int }
+
+
+// OutputBoundsOf consumes a Cli value and returns pixel width, height tuple
+func OutputDimsOf(scales ScaleFactors, img image.Image) (w, h uint) {
+	height := float64(uint(img.Bounds().Max.Y))
+	width := float64(uint(img.Bounds().Max.X))
+
+	scale := scales.GetScale()
+	ratio := width / height * scales.GetSquash()
+
+	return uint(scale * height * ratio), uint(scale * height)
+}
+
+// ScaleImg does global scale and makes boyz wide
+func ScaleImg(img image.Image, sf ScaleFactors) image.Image {
+	w, h := OutputDimsOf(sf, img)
+	return resize.Resize(w, h, img, resize.Lanczos2)
+}
